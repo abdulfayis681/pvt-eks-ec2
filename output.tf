16f9179d224f4fb726f3d34b0b359540ddc79a55
abdulfayis681 <103239046+abdulfayis681@users.noreply.github.com>
2024-01-31 15:56:30 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -13,22 +13,10 @@ output "private_subnet_ids" {
 output "eks_cluster_name" {
   value = module.eks_cluster.eks_cluster_id
 }
-output "bastion_ip" {
-  value = module.bastion.public_ip
-}
-output "bastion_id" {
-  value = module.bastion.id
-}
+
 output "jenkins_server_ip" {
   value = module.jenkins_server.private_ip
 }
 output "jenkins_server_id" {
   value = module.jenkins_server.id
 
-}
-output "sonarqube_ip" {
-  value = module.sonarqube.public_ip
-}
-output "sonarqube_id" {
-  value = module.sonarqube.id
-}
